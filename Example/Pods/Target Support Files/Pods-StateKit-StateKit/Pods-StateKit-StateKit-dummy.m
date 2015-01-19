11e648516d118afec9fa6ff4234d0b50571396da
Shaheen Ghiassy <shaheen@groupon.com>
2015-01-19 11:54:13 -0800

    Initial Commit

---
+++ Example/Pods/Target Support Files/Pods-StateKit-StateKit/Pods-StateKit-StateKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StateKit_StateKit : NSObject
+@end
+@implementation PodsDummy_Pods_StateKit_StateKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-StateKit/Pods-StateKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StateKit : NSObject
+@end
+@implementation PodsDummy_Pods_StateKit
+@end
