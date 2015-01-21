c3c8a4ba0956ac19e28aef1a1d7df87bea3ffdad
Shaheen Ghiassy <shaheen@groupon.com>
2015-01-21 14:58:13 -0800

    Added more tests

--- Example/Tests/Message_Tests.m
+++ Example/Tests/Message_Tests.m
@@ -35,6 +35,29 @@ describe(@"SKStateMachine", ^{
 
         expect(mock).to.equal(@"waka_waka was called");
     });
+
+    it(@"Methods in the same state, will not respond", ^{
+        __block NSString *waka = @"not called";
+        __block NSString *yaka = @"not called";
+
+        NSDictionary *chart = @{@"root":@{
+                                        @"waka_waka":^(SKStateChart *sc){
+                                            waka = @"waka_waka was called";
+                                        },
+                                        @"yaka_yaka":^(SKStateChart *sc) {
+                                            yaka = @"yaka_yaka was called";
+                                        }}};
+
+        SKStateChart *stateChart = [[SKStateChart alloc] initWithStateChart:chart];
+
+        expect(waka).to.equal(@"not called");
+        expect(yaka).to.equal(@"not called");
+
+        [stateChart sendMessage:@"waka_waka"];
+
+        expect(waka).to.equal(@"waka_waka was called");
+        expect(yaka).to.equal(@"not called");
+    });
     
 });
 
