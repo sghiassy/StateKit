57182880be7a4328f3f5a25fc465834a7718a051
Shaheen Ghiassy <shaheen@groupon.com>
2015-01-21 15:47:13 -0800

    More Tests

--- Example/Tests/Message_Tests.m
+++ Example/Tests/Message_Tests.m
@@ -58,6 +58,40 @@ describe(@"SKStateMachine", ^{
         expect(waka).to.equal(@"waka_waka was called");
         expect(yaka).to.equal(@"not called");
     });
+
+    it(@"Messages in substates with the same name will not respond", ^{
+        __block NSString *waka = @"not called";
+        __block NSString *yaka = @"not called";
+
+        NSDictionary *chart = @{@"root":@{
+                                        @"waka_waka":^(SKStateChart *sc){
+                                            // Enforce the fact that we are not overriding the substates value
+                                            if ([waka isEqualToString:@"not called"]) {
+                                                waka = @"waka_waka was called";
+                                            }
+                                        },
+                                        @"yaka_yaka":^(SKStateChart *sc) {
+                                            yaka = @"yaka_yaka was called";
+                                        },
+                                        @"subStates":@{
+                                                @"rootChildState":@{
+                                                        @"waka_waka":^(SKStateChart *sc) {
+                                                            waka = @"waka_waka_sub_state was called";
+                                                        }}
+                                                }
+                                        }
+                                };
+
+        SKStateChart *stateChart = [[SKStateChart alloc] initWithStateChart:chart];
+
+        expect(waka).to.equal(@"not called");
+        expect(yaka).to.equal(@"not called");
+
+        [stateChart sendMessage:@"waka_waka"];
+
+        expect(waka).to.equal(@"waka_waka was called");
+        expect(yaka).to.equal(@"not called");
+    });
     
 });
 
