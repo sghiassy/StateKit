3bd5466f1cde78b129a708b6d7e58db7e891c7c3
Shaheen Ghiassy <shaheen@groupon.com>
2015-01-22 16:47:10 -0800

    Cleaned up tests

--- Example/Tests/Message_Tests.m
+++ Example/Tests/Message_Tests.m
@@ -8,88 +8,74 @@
 
 #import <SKStateChart.h>
 
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+#define MOCKITO_SHORTHAND
+#import <OCMockito/OCMockito.h>
+
+#import "MockLogger.h"
+
 SpecBegin(StateChart_Messages)
 
 describe(@"SKStateMachine", ^{
 
+    __block SKStateChart *stateChart;
+    __block id logMock;
+
+    NSDictionary *chart = @{@"root":@{
+                                    @"waka_waka":^(SKStateChart *sc){
+                                        [logMock log:@"waka_waka was called"];
+                                    },
+                                    @"yaka_yaka":^(SKStateChart *sc) {
+                                        [logMock log:@"yaka_yaka was called"];
+                                    },
+                                    @"subStates":@{
+                                            @"rootChildState":@{
+                                                    @"waka_waka":^(SKStateChart *sc) {
+                                                        [logMock log:@"waka_waka_sub_state was called"];
+                                                    }}
+                                            }
+                                    }
+                            };
+
+    beforeEach(^{
+        logMock = mock([MockLogger class]);
+        stateChart = [[SKStateChart alloc] initWithStateChart:chart];
+    });
+
     it(@"can be instantiated", ^{
-        SKStateChart *stateChart = [[SKStateChart alloc] init];
         expect(stateChart).to.beTruthy();
     });
 
     it(@"The current state will respond to events", ^{
-        __block NSString *mock = @"not called";
-
-        NSDictionary *chart = @{@"root":@{
-                                        @"waka_waka":^(SKStateChart *sc){
-                                            mock = @"waka_waka was called";
-                                        }
-                                        }};
-
-        SKStateChart *stateChart = [[SKStateChart alloc] initWithStateChart:chart];
-
-        expect(mock).to.equal(@"not called");
+        [verifyCount(logMock, times(0)) log:@"waka_waka was called"];
 
         [stateChart sendMessage:@"waka_waka"];
 
-        expect(mock).to.equal(@"waka_waka was called");
+        [verifyCount(logMock, times(1)) log:@"waka_waka was called"];
     });
 
     it(@"Methods in the same state, will not respond", ^{
-        __block NSString *waka = @"not called";
-        __block NSString *yaka = @"not called";
-
-        NSDictionary *chart = @{@"root":@{
-                                        @"waka_waka":^(SKStateChart *sc){
-                                            waka = @"waka_waka was called";
-                                        },
-                                        @"yaka_yaka":^(SKStateChart *sc) {
-                                            yaka = @"yaka_yaka was called";
-                                        }}};
-
-        SKStateChart *stateChart = [[SKStateChart alloc] initWithStateChart:chart];
-
-        expect(waka).to.equal(@"not called");
-        expect(yaka).to.equal(@"not called");
+        [verifyCount(logMock, times(0)) log:@"waka_waka was called"];
+        [verifyCount(logMock, times(0)) log:@"yaka_yaka was called"];
 
         [stateChart sendMessage:@"waka_waka"];
 
-        expect(waka).to.equal(@"waka_waka was called");
-        expect(yaka).to.equal(@"not called");
+        [verifyCount(logMock, times(1)) log:@"waka_waka was called"];
+        [verifyCount(logMock, times(0)) log:@"yaka_yaka was called"];
     });
 
     it(@"Messages in substates with the same name will not respond", ^{
-        __block NSString *waka = @"not called";
-        __block NSString *yaka = @"not called";
-
-        NSDictionary *chart = @{@"root":@{
-                                        @"waka_waka":^(SKStateChart *sc){
-                                            // Enforce the fact that we are not overriding the substates value
-                                            if ([waka isEqualToString:@"not called"]) {
-                                                waka = @"waka_waka was called";
-                                            }
-                                        },
-                                        @"yaka_yaka":^(SKStateChart *sc) {
-                                            yaka = @"yaka_yaka was called";
-                                        },
-                                        @"subStates":@{
-                                                @"rootChildState":@{
-                                                        @"waka_waka":^(SKStateChart *sc) {
-                                                            waka = @"waka_waka_sub_state was called";
-                                                        }}
-                                                }
-                                        }
-                                };
-
-        SKStateChart *stateChart = [[SKStateChart alloc] initWithStateChart:chart];
-
-        expect(waka).to.equal(@"not called");
-        expect(yaka).to.equal(@"not called");
+        [verifyCount(logMock, times(0)) log:@"waka_waka was called"];
+        [verifyCount(logMock, times(0)) log:@"yaka_yaka was called"];
+        [verifyCount(logMock, times(0)) log:@"waka_waka_sub_state was called"];
 
         [stateChart sendMessage:@"waka_waka"];
 
-        expect(waka).to.equal(@"waka_waka was called");
-        expect(yaka).to.equal(@"not called");
+        [verifyCount(logMock, times(1)) log:@"waka_waka was called"];
+        [verifyCount(logMock, times(0)) log:@"yaka_yaka was called"];
+        [verifyCount(logMock, times(0)) log:@"waka_waka_sub_state was called"];
     });
     
 });
